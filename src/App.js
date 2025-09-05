9c531a242b59275b916612ce395812eacd92ed5a
Rhitree <rhian.jenkins90@gmail.com>
2025-09-05 14:25:43 +0100

    Added Footer

--- src/App.js
+++ src/App.js
@@ -10,5 +10,8 @@ function App() {
     </div>
   );
 }
+  <footer>
+      This project was coded by Rhian Jenkins & is open-sourced on <a href="https://github.com/Rhitree/weather-app-react">GitHub</a> hosted on <a href="https://enchanting-daffodil-161e3c.netlify.app/">Netlify</a>
+    </footer>
 
 export default App;
